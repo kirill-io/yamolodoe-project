86741fe2cd779d08ac23d096592e58cfb5f4cc4e
Kirill Begej <fleki679@yandex.ru>
2025-03-21 20:15:02 +0300

    feat: добавить секцию about в desktop-версии

--- src/js/utils/constants.js
+++
@@ -1,9 +0,0 @@
-export const INTRO_SLIDER_DEFAULT = {
-  transitionDuration: 50,
-  containerSelector: '.intro__slider-list',
-  itemSelector: '.intro__slide',
-  dotItemSelector: '.dots__item',
-  dotItemActionSelector: 'dots__item_active',
-  slidesAmountVar: '--slides',
-  currentSlideVar: '--current',
-};
