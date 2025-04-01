d22cc2b60b7356c847d389af8c7867a038282c79
Kirill Begej <fleki679@yandex.ru>
2025-04-01 21:03:01 +0300

    feat: добавить секцию map страницы contacts.html

--- src/js/pages/contacts.js
+++ src/js/pages/contacts.js
@@ -1,1 +1,5 @@
 import 'css/pages/contacts.css';
+import * as constants from '../utils/constants';
+import StickyHeader from '../components/StickyHeader';
+
+new StickyHeader(StickyHeader.init(constants.headerConfig));
