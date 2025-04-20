3153a01ed6c2c9fbed57c5e5a7f3ca38cc582169
Kirill Begej <fleki679@yandex.ru>
2025-04-20 18:27:46 +0300

    fix: тест мобильной версии

--- src/js/components/PreloaderPage.js
+++ src/js/components/PreloaderPage.js
@@ -19,7 +19,7 @@ export default class PreloaderPage {
   }
 
   #hidePreloader() {
-    this._preloader.style.display = 'none';
+    // this._preloader.style.display = 'none';
   }
 
   #showContent() {
